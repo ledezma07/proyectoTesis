d3ab4cc5397dfc172b80712f960ef7512b491dce
ledezma07 <lezma_07@hotmail.com>
2023-12-28 17:46:36 -0400

    Todo Fronten

---
+++ proyecto/index.php
@@ -0,0 +1,298 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+
+    <meta name="generator" content="Hugo 0.118.2">
+    <title>SUPER MULTITEATRO</title>
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+  </head>
+  <body>
+   
+<!-- 
+  PARA LA NAVEGACION
+-->
+ 
+<main>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">SUPER MULTITEATRO ORURO</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample05">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">INICIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ENLACES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">CARTELERA</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">A QUE NOS DEDICAMOS</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">TEATRO</a></li>
+              <li><a class="dropdown-item" href="#">MUSICA</a></li>
+              <li><a class="dropdown-item" href="#">DANZA</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">INICIAR SESION</a>
+          </li>
+        </ul>
+        <form role="search">
+        <input class="form-control" type="search" placeholder="BUSCAR" aria-label="Search">
+        </form>
+        
+      </div>
+    </div>
+  </nav>
+</main>
+<!-- 
+  FIN PARA LA NAVEGACION
+-->
+ 
+
+<!--incio de las tarjetas
+-->
+
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Cartelarea de Funciones</h1>
+        <p class="lead text-body-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ipsa recusandae suscipit, voluptatem provident consectetur autem enim asperiores! Debitis voluptas excepturi eveniet deserunt placeat? Dolore, maiores sint? Quas, quibusdam omnis?  <p>
+          <a href="#" class="btn btn-primary my-2">VOLVER</a>
+          <a href="#" class="btn btn-secondary my-2">VOLVER 2</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-body-tertiary">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+          <img class="mb-4" src="../assets/brand/tar-03.png" alt="" wwidth="100%" height="225">
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+           
+            <img class="mb-4" src="../assets/brand/tar-01.png" alt="" wwidth="100%" height="225">
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img class="mb-4" src="../assets/brand/tar-02.png" alt="" wwidth="100%" height="225">
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+          <img class="mb-4" src="../assets/brand/tar-04.png" alt="" wwidth="100%" height="225">
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img class="mb-4" src="../assets/brand/tar-05.png" alt="" wwidth="100%" height="225">
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img class="mb-4" src="../assets/brand/tar-06.png" alt="" wwidth="100%" height="225">  
+          <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: imagen" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">imagen123</text></svg>
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: imagen" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">imagen123</text></svg>
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: imagen" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">imagen123</text></svg>
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae maiores numquam quidem! Nemo voluptates quibusdam nostrum. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON2</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">BOTON1</button>
+                </div>
+                <small class="text-body-secondary">9BOTON</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<!-- 
+  FIN PARA LAS TARJETAS
+-->
+
+<div class="b-example-divider"></div>
+
+<div class="container">
+  <footer class="py-5">
+    <div class="row">
+      <div class="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+
+      <div class="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+
+      <div class="col-6 col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-5 offset-md-1 mb-3">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of what's new and exciting from us.</p>
+          <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+            <label for="newsletter1" class="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+            <button class="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p>&copy; 2023 Company, Inc. All rights reserved.</p>
+      <ul class="list-unstyled d-flex">
+        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
---
+++ proyecto/login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="generator" content="Hugo 0.118.2">
+    <title>LOGIN</title>
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="../assets/estilos/sign-in.css" rel="stylesheet">
+  </head>
+  <body class="d-flex align-items-center py-4 bg-body-tertiary">
+  
+<main class="form-signin w-100 m-auto">
+  <form>
+    <img class="mb-4" src="../assets/brand/logo.png" alt="" width="80" height="80">
+    <h1 class="h3 mb-3 fw-normal">Inicio de Sesion</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Usuario</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Contraseña</label>
+    </div>
+
+    <div class="form-check text-start my-3">
+      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        Recordarme
+      </label>
+    </div>
+    <button class="btn btn-primary w-100 py-2" type="submit">Ingresar</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; Ledezma–2024
+      <a href="index.php">Inicio</a>
+    </p>
+  </form>
+</main>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
